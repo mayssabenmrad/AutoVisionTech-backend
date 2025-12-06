f0d6b89978270710a04de9ceeafc75a47b344e1f
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-12-06 02:23:52 +0100

    feat(comment): add controller and update module

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,34 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  /*UseGuards,*/ Param,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+//import { PermissionsGuard } from 'src/auth/guards/permissions.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@Controller('comment')
+@Controller('comments')
+//@UseGuards(PermissionsGuard)
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
+  @Public()
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.commentService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  @Get('car/:carId')
+  @Public()
+  findByCarId(@Param('carId') carId: string) {
+    return this.commentService.findByCarId(carId);
   }
 }
--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { CommentEntity } from './comment.entity';
+import { CarEntity } from '../car/car.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommentEntity, CarEntity])],
   controllers: [CommentController],
   providers: [CommentService],
 })
