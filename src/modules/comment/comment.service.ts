889b12baf9be3eb40b7da66b4ffa0ddfc53b95ab
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-12-06 02:21:14 +0100

    feat(comment): add create comment service

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comment.entity';
+import { Repository } from 'typeorm';
+import { CarEntity } from '../car/car.entity';
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(CommentEntity)
+    private readonly commentRepository: Repository<CommentEntity>,
+    @InjectRepository(CarEntity)
+    private readonly carRepository: Repository<CarEntity>,
+  ) {}
+
+  //Create a comment
+  async create(commentData: CreateCommentDto): Promise<CommentEntity | null> {
+    const car = await this.carRepository.findOne({
+      where: { id: commentData.carId },
+    });
+
+    if (!car) {
+      throw new NotFoundException('Car Not Found');
+    }
+
+    const comment = this.commentRepository.create({ ...commentData });
+    return this.commentRepository.save(comment);
   }
 
-  findAll() {
-    return `This action returns all comment`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
-
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  //retrieve all comments for a specific car
+  async findByCarId(carId: string): Promise<CommentEntity[]> {
+    return await this.commentRepository.find({ where: { carId } });
   }
 }
